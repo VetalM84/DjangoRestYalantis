7aef301e53a5b87fd21052c4409cf5876b6d3bb5
Виталик <vetalm@i.ua>
2021-12-09 13:01:06 +0200

    GET url params implemented

--- car_park/views.py
+++ car_park/views.py
@@ -8,13 +8,22 @@ from .models import Driver, Vehicle
 from .serializers import DriverSerializer, VehicleSerializer
 
 
-class DriverList(generics.ListAPIView):
+class DriverList(generics.ListCreateAPIView):
     permission_classes = (AllowAny,)
-    queryset = Driver.objects.all()
     serializer_class = DriverSerializer
 
+    def get_queryset(self):
+        queryset = Driver.objects.all()
+        date_gte = self.request.query_params.get('created_at__gte')
+        date_lte = self.request.query_params.get('created_at__lte')
+        if date_gte is not None:
+            queryset = queryset.filter(created_at__gte=date_gte)
+        elif date_lte is not None:
+            queryset = queryset.filter(created_at__lte=date_lte)
+        return queryset
 
-class DriverDetail(generics.RetrieveAPIView):
+
+class DriverDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (AllowAny,)
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
@@ -22,8 +31,16 @@ class DriverDetail(generics.RetrieveAPIView):
 
 class VehicleList(generics.ListCreateAPIView):
     permission_classes = (AllowAny,)
-    queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+
+    def get_queryset(self):
+        queryset = Vehicle.objects.all()
+        with_drivers = self.request.query_params.get('with_drivers')
+        if with_drivers == 'yes':
+            queryset = queryset.filter(driver_id__isnull=False)
+        elif with_drivers == 'no':
+            queryset = queryset.filter(driver_id__isnull=True)
+        return queryset
 
 
 class VehicleDetail(generics.RetrieveUpdateDestroyAPIView):
